ed314cffc31f16341f26c34a4648add7f6bae069
binarybytes <31395956+binarybytes@users.noreply.github.com>
2024-02-05 21:32:53 -0500

    Update csvparser.py

--- csvparser.py
+++ csvparser.py
@@ -9,10 +9,19 @@ with zipfile.ZipFile("sheet.zip") as z:
        # read the dataset
         df = pd.read_csv(f)
 
-#search for string within specific column
-print(df[df.payload.str.contains('evil')])
+#print(df[df.payload.str.contains('evil')])
+#print(df[df.payload.str.contains('re|evil', case=False, regex=True)])
 
-#search using regex within specific column
-print(df[df.payload.str.contains('re|evil', case=False, regex=True)])
+#define a search function
+def search_string(s, search):
+    return search in str(s).lower()
+
+# Search for the string 'al' in all columns
+mask = df.apply(lambda x: x.map(lambda s: search_string(s, '.php')))
+
+# Filter the DataFrame based on the mask
+filtered_df = df.loc[mask.any(axis=1)]
+print(filtered_df)
+
 
 #python -W ignore test.py
