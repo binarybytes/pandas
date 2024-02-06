8a29b3dab9a4a8227876050a5df678a0b100d391
binarybytes <31395956+binarybytes@users.noreply.github.com>
2024-02-05 21:14:58 -0500

    Create csvparser.py

---
+++ csvparser.py
@@ -0,0 +1,13 @@
+# import required modules
+import zipfile
+import pandas as pd
+ 
+# open zipped dataset
+with zipfile.ZipFile("sheet.zip") as z:
+   # open the csv file in the dataset
+   with z.open("sheet.csv") as f:
+       # read the dataset
+        df = pd.read_csv(f)
+
+#print(df[df.payload.str.contains('evil')])
+print(df[df.payload.str.contains('re|evil', case=False, regex=True)])
